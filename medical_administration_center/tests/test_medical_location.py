2fb3bc75732b4435a3f64e44b3cde089394d1ce9
Enric Tobella <etobella@creublanca.es>
2018-05-07 16:45:24 +0200

    [IMP] tests

--- medical_administration_center/tests/test_medical_location.py
+++ medical_administration_center/tests/test_medical_location.py
@@ -4,11 +4,19 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo.tests.common import TransactionCase
+from odoo.exceptions import ValidationError
 
 
 class TestMedicalLocation(TransactionCase):
 
-    def test_location(self):
+    def test_center(self):
+        vals = {
+            'name': 'location',
+            'description': 'location_description',
+            'is_location': True,
+            }
+        with self.assertRaises(ValidationError):
+            self.env['res.partner'].create(vals)
         center_vals = {
             'name': 'test name',
             'description': 'test description',
@@ -16,3 +24,10 @@ class TestMedicalLocation(TransactionCase):
         }
         center = self.env['res.partner'].create(center_vals)
         self.assertTrue(center.is_center)
+        self.assertTrue(center.center_identifier)
+        vals['center_id'] = center.id
+        location = self.env['res.partner'].create(vals)
+        self.assertTrue(location.is_location)
+        vals['is_location'] = False
+        with self.assertRaises(ValidationError):
+            self.env['res.partner'].create(vals)
