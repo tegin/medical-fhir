fe1af9bd580880eb55e5026240c2dbb61a3d41ec
Enric Tobella <etobella@creublanca.es>
2018-07-11 09:03:12 +0200

    fixup! Fix travis

--- medical_clinical_procedure/tests/test_medical_request.py
+++ medical_clinical_procedure/tests/test_medical_request.py
@@ -41,8 +41,9 @@ class TestMedicalRequest(TransactionCase):
         })
         procedure._compute_procedure_request_ids()
         self.assertEqual(procedure.procedure_request_count, 2)
-        self.assertEqual(procedure.procedure_request_ids.ids, [procedure2.id,
-                                                               procedure3.id])
+        self.assertListEqual(
+            procedure.procedure_request_ids.ids,
+            [procedure2.id,procedure3.id])
         procedure.with_context(
             inverse_id='active_id', model_name='medical.procedure.request')\
             .action_view_request()
