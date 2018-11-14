52c1f13f484f14804dfb86a7d38c36da69d21c55
Enric Tobella <etobella@creublanca.es>
2018-11-14 15:14:45 +0100

    [FIX] constrains

--- medical_medication_request/tests/test_medical_request.py
+++ medical_medication_request/tests/test_medical_request.py
@@ -15,34 +15,48 @@ class TestMedicalRequest(TransactionCase):
         self.patient2 = self.env['medical.patient'].create({
             'name': 'Test Patient2'
         })
-
-    def test_constrains(self):
-        request = self.env['medical.medication.request'].create({
-            'patient_id': self.patient.id
-        })
-        with self.assertRaises(ValidationError):
-            self.env['medical.medication.request'].create({
-                'patient_id': self.patient2.id,
-                'medication_request_id': request.id,
-            })
-
-    def test_constrains_administration(self):
-        request = self.env['medical.medication.request'].create({
-            'patient_id': self.patient.id
-        })
-        with self.assertRaises(ValidationError):
-            self.env['medical.medication.administration'].create({
-                'patient_id': self.patient2.id,
-                'medication_request_id': request.id,
-            })
-
-    def test_views(self):
         self.uom_unit = self.env.ref('product.product_uom_unit')
         self.medication = self.env['product.product'].create({
             'name': 'Medication',
             'is_medication': True,
             'type': 'consu',
         })
+
+    def test_constrains(self):
+        request = self.env['medical.medication.request'].create({
+            'patient_id': self.patient.id,
+            'product_id': self.medication.id,
+            'product_uom_id': self.uom_unit.id,
+            'qty': 1,
+        })
+        with self.assertRaises(ValidationError):
+            self.env['medical.medication.request'].create({
+                'patient_id': self.patient2.id,
+                'medication_request_id': request.id,
+                'product_id': self.medication.id,
+                'product_uom_id': self.uom_unit.id,
+                'qty': 1,
+            })
+
+    def test_constrains_administration(self):
+        request = self.env['medical.medication.request'].create({
+            'patient_id': self.patient.id,
+            'product_id': self.medication.id,
+            'product_uom_id': self.uom_unit.id,
+            'qty': 1,
+
+        })
+        with self.assertRaises(ValidationError):
+            self.env['medical.medication.administration'].create({
+                'patient_id': self.patient2.id,
+                'medication_request_id': request.id,
+                'product_id': self.medication.id,
+                'product_uom_id': self.uom_unit.id,
+                'qty': 1,
+
+            })
+
+    def test_views(self):
         # medication request
         medication_request = self.env['medical.medication.request'].create({
             'patient_id': self.patient.id,
