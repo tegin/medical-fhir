558796d7dcc88ed7da7fcb88690a0eb4e5307b23
Enric Tobella <etobella@creublanca.es>
2018-11-14 11:47:07 +0100

    [IMP] Check patient

--- medical_clinical_request_group/tests/test_medical_request.py
+++ medical_clinical_request_group/tests/test_medical_request.py
@@ -3,6 +3,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo.tests.common import TransactionCase
+from odoo.exceptions import ValidationError
 
 
 class TestMedicalRequest(TransactionCase):
@@ -12,6 +13,20 @@ class TestMedicalRequest(TransactionCase):
         self.patient = self.env['medical.patient'].create({
             'name': 'Test Patient'
         })
+        self.patient2 = self.env['medical.patient'].create({
+            'name': 'Test Patient2'
+        })
+
+    def test_constrains(self):
+        # care plans
+        request = self.env['medical.request.group'].create({
+            'patient_id': self.patient.id
+        })
+        with self.assertRaises(ValidationError):
+            self.env['medical.request.group'].create({
+                'patient_id': self.patient2.id,
+                'request_group_id': request.id,
+            })
 
     def test_views(self):
         # procedure
