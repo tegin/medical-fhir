0915803ce8cbb92d20fe22c06c5333e96f1a08c6
Enric Tobella <etobella@creublanca.es>
2018-12-16 01:43:10 +0100

    [IMP] Change the code of stock with the standard code

---
+++ medical_medication_request/models/procurement.py
@@ -0,0 +1,18 @@
+from odoo import models
+
+
+class ProcurementRule(models.Model):
+    _inherit = 'procurement.rule'
+
+    def _get_stock_move_values(
+        self, product_id, product_qty, product_uom, location_id, name,
+        origin, values, group_id
+    ):
+        res = super()._get_stock_move_values(
+            product_id, product_qty, product_uom, location_id, name,
+            origin, values, group_id
+        )
+        if values.get('medication_administration_id', False):
+            res['medication_administration_id'] = values.get(
+                'medication_administration_id')
+        return res
