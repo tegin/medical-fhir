e61e5a4b36d8e9c64bdb566f7584bbeac79b0d98
Enric Tobella <etobella@creublanca.es>
2018-12-31 16:10:21 +0100

    [FIX] security

--- medical_medication_request/models/__init__.py
+++ medical_medication_request/models/__init__.py
@@ -2,7 +2,6 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-#from . import medical_encounter
 from . import medical_request
 from . import medical_medication_administration
 from . import medical_medication_request
