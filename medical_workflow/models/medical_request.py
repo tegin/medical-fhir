3a1a42ee4a99dc11f7e0e68db1e772e5e3e77587
Enric Tobella <etobella@creublanca.es>
2019-02-18 14:00:35 +0100

    [FIX] check_patient

--- medical_workflow/models/medical_request.py
+++ medical_workflow/models/medical_request.py
@@ -252,7 +252,7 @@ class MedicalRequest(models.AbstractModel):
                 for model in models:
                     if self.env[model].search([
                         (fieldname, '=', r.id),
-                        ('patient_id', '=', r.id),
+                        ('patient_id', '!=', r.patient_id.id),
                     ], limit=1):
                         raise ValidationError(_('Patient must be consistent'))
                 for parent in r._get_parents():
