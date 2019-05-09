11518d8af311c9d6233d94de8ec8f8a1cfd7fe23
Enric Tobella <etobella@creublanca.es>
2019-05-09 09:22:27 +0200

    [FIX] Author

--- medical_administration_center/__manifest__.py
+++ medical_administration_center/__manifest__.py
@@ -1,4 +1,3 @@
-# Copyright 2017 LasLabs Inc.
 # Copyright 2017 Creu Blanca
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
@@ -8,8 +7,7 @@
     "version": "11.0.1.0.0",
     "category": "Medical",
     "website": "https://github.com/OCA/vertical-medical",
-    "author": "LasLabs, Creu Blanca, Eficent,"
-              "Odoo Community Association (OCA)",
+    "author": "Creu Blanca, Eficent",
     "license": "LGPL-3",
     "depends": [
         'medical_administration_location',
