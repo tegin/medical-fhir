3f5636628c6ce06fc4da1b9f1b8931aa6b787b44
Enric Tobella <etobella@creublanca.es>
2019-05-29 07:39:56 +0200

    [MIG] Migration to 12.0

--- medical_medication/__manifest__.py
+++ medical_medication/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Medical Medication',
     'summary': 'Medical medication base',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
     'category': 'Medical',
     'website': 'https://github.com/OCA/vertical-medical',
