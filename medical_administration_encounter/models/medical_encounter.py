c591397e9fa3a024b3cb2b535c8d3c5f2fe870cc
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_administration_encounter/models/medical_encounter.py
+++ medical_administration_encounter/models/medical_encounter.py
@@ -7,38 +7,34 @@ from odoo import api, fields, models
 
 class MedicalEncounter(models.Model):
     # FHIR Entity: Encounter (https://www.hl7.org/fhir/encounter.html)
-    _name = 'medical.encounter'
-    _description = 'Medical Encounter'
-    _inherit = ['medical.abstract', 'mail.thread', 'mail.activity.mixin']
-    _order = 'create_date DESC'
+    _name = "medical.encounter"
+    _description = "Medical Encounter"
+    _inherit = ["medical.abstract", "mail.thread", "mail.activity.mixin"]
+    _order = "create_date DESC"
 
-    name = fields.Char(
-        string='Name',
-    )
-    internal_identifier = fields.Char(
-        string="Encounter"
-    )
+    name = fields.Char(string="Name")
+    internal_identifier = fields.Char(string="Encounter")
     patient_id = fields.Many2one(
-        string='Patient',
-        comodel_name='medical.patient',
+        string="Patient",
+        comodel_name="medical.patient",
         required=True,
         track_visibility=True,
-        ondelete='restrict', index=True,
-        help='Patient name',
+        ondelete="restrict",
+        index=True,
+        help="Patient name",
     )  # FHIR Field: subject
     priority_id = fields.Selection(
         string="Priority",
-        selection=[
-            ("UR", "Urgent")
-        ],
+        selection=[("UR", "Urgent")],
         help="Indicates the urgency of the encounter.",
     )  # FHIR Field: priority
     location_id = fields.Many2one(
         string="Location",
-        comodel_name='res.partner',
-        domain=[('is_location', '=', True)],
+        comodel_name="res.partner",
+        domain=[("is_location", "=", True)],
         track_visibility=True,
-        ondelete='restrict', index=True,
+        ondelete="restrict",
+        index=True,
     )  # FHIR Field: location
     state = fields.Selection(
         string="Encounter Status",
@@ -50,91 +46,93 @@ class MedicalEncounter(models.Model):
             ("in-progress", "In-Progress"),
             ("onleave", "On Leave"),
             ("finished", "Finished"),
-            ("cancelled", "Cancelled")],
+            ("cancelled", "Cancelled"),
+        ],
         default="arrived",
         help="Current state of the encounter.",
     )  # FHIR Field: status
-    is_editable = fields.Boolean(
-        compute='_compute_is_editable',
-    )
+    is_editable = fields.Boolean(compute="_compute_is_editable")
 
     @api.model
     def _get_internal_identifier(self, vals):
-        return self.env['ir.sequence'].next_by_code(
-            'medical.encounter') or '/'
+        return self.env["ir.sequence"].next_by_code("medical.encounter") or "/"
 
     @api.multi
-    @api.depends('name', 'internal_identifier')
+    @api.depends("name", "internal_identifier")
     def name_get(self):
         result = []
         for record in self:
-            name = '[%s]' % record.internal_identifier
+            name = "[%s]" % record.internal_identifier
             if record.name:
-                name = '%s %s' % (name, record.name)
+                name = "%s %s" % (name, record.name)
             result.append((record.id, name))
         return result
 
     @api.multi
-    @api.depends('state')
+    @api.depends("state")
     def _compute_is_editable(self):
         for rec in self:
-            if rec.state in \
-                    ('in-progress', 'onleave', 'finished', 'cancelled'):
+            if rec.state in (
+                "in-progress",
+                "onleave",
+                "finished",
+                "cancelled",
+            ):
                 rec.is_editable = False
             else:
                 rec.is_editable = True
 
     def planned2arrived_values(self):
-        return {'state': 'arrived'}
+        return {"state": "arrived"}
 
     @api.multi
     def planned2arrived(self):
         self.write(self.planned2arrived_values())
 
     def planned2cancelled_values(self):
-        return {'state': 'cancelled'}
+        return {"state": "cancelled"}
 
     @api.multi
     def planned2cancelled(self):
         self.write(self.planned2cancelled_values())
 
     def arrived2inprogress_values(self):
-        return {'state': 'in-progress'}
+        return {"state": "in-progress"}
 
     @api.multi
     def arrived2inprogress(self):
         self.write(self.arrived2inprogress_values())
 
     def arrived2cancelled_values(self):
-        return {'state': 'cancelled'}
+        return {"state": "cancelled"}
 
     @api.multi
     def arrived2cancelled(self):
         self.write(self.arrived2cancelled_values())
 
     def inprogress2onleave_values(self):
-        return {'state': 'onleave'}
+        return {"state": "onleave"}
 
     @api.multi
     def inprogress2onleave(self):
         self.write(self.inprogress2onleave_values())
 
     def inprogress2cancelled_values(self):
-        return {'state': 'cancelled'}
+        return {"state": "cancelled"}
 
     @api.multi
     def inprogress2cancelled(self):
         self.write(self.inprogress2cancelled_values())
 
     def onleave2finished_values(self):
-        return {'state': 'finished'}
+        return {"state": "finished"}
 
     @api.multi
     def onleave2finished(self):
         self.write(self.onleave2finished_values())
 
     def onleave2cancelled_values(self):
-        return {'state': 'cancelled'}
+        return {"state": "cancelled"}
 
     @api.multi
     def onleave2cancelled(self):
