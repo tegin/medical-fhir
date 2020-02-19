01e5803b80c054ffcb8078e79804fc1eea539362
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_clinical_condition/tests/test_condition.py
+++ medical_clinical_condition/tests/test_condition.py
@@ -8,14 +8,13 @@ from odoo.tests.common import TransactionCase
 class TestCondition(TransactionCase):
     def setUp(self):
         super(TestCondition, self).setUp()
-        self.patient = self.env['medical.patient'].create({
-            'name': 'Patient'
-        })
-        self.finding = self.env['medical.clinical.finding'].create({
-            'name': 'Finding'
-        })
+        self.patient = self.env["medical.patient"].create({"name": "Patient"})
+        self.finding = self.env["medical.clinical.finding"].create(
+            {"name": "Finding"}
+        )
         self.pregnant = self.browse_ref(
-            'medical_clinical_condition.finding_pregnant')
+            "medical_clinical_condition.finding_pregnant"
+        )
 
     def test_pregnant(self):
         self.assertFalse(self.patient.is_pregnant)
@@ -27,12 +26,14 @@ class TestCondition(TransactionCase):
 
     def test_conditions(self):
         self.assertEqual(self.patient.medical_condition_count, 0)
-        self.env['medical.condition'].create({
-            'patient_id': self.patient.id,
-            'clinical_finding_id': self.finding.id,
-        })
+        self.env["medical.condition"].create(
+            {
+                "patient_id": self.patient.id,
+                "clinical_finding_id": self.finding.id,
+            }
+        )
         self.assertEqual(self.patient.medical_condition_count, 1)
         self.patient.toggle_is_pregnant()
         self.assertEqual(self.patient.medical_condition_count, 2)
         res = self.patient.action_view_medical_conditions()
-        self.assertEqual(res['context']['default_patient_id'], self.patient.id)
+        self.assertEqual(res["context"]["default_patient_id"], self.patient.id)
