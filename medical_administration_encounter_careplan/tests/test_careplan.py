193b318f1c6e56ae09e867e274a69fbda7cee9bd
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_administration_encounter_careplan/tests/test_careplan.py
+++ medical_administration_encounter_careplan/tests/test_careplan.py
@@ -9,51 +9,45 @@ from odoo.exceptions import ValidationError
 class TestEncounter(TransactionCase):
     def setUp(self):
         super(TestEncounter, self).setUp()
-        self.patient = self.env['medical.patient'].create({
-            'name': 'Patient'
-        })
-        self.patient_2 = self.env['medical.patient'].create({
-            'name': 'Patient 2'
-        })
-        self.plan = self.browse_ref('medical_workflow.mr_knee')
+        self.patient = self.env["medical.patient"].create({"name": "Patient"})
+        self.patient_2 = self.env["medical.patient"].create(
+            {"name": "Patient 2"}
+        )
+        self.plan = self.browse_ref("medical_workflow.mr_knee")
 
     def test_create_careplan_constrains(self):
-        encounter = self.env['medical.encounter'].create({
-            'patient_id': self.patient.id
-        })
+        encounter = self.env["medical.encounter"].create(
+            {"patient_id": self.patient.id}
+        )
         self.assertEqual(encounter.careplan_count, 0)
         res = encounter.action_view_careplans()
-        self.assertFalse(res.get('res_id'))
-        careplan = self.env['medical.careplan'].create({
-            'patient_id': self.patient.id,
-            'encounter_id': encounter.id
-        })
+        self.assertFalse(res.get("res_id"))
+        careplan = self.env["medical.careplan"].create(
+            {"patient_id": self.patient.id, "encounter_id": encounter.id}
+        )
         self.assertEqual(encounter.careplan_count, 1)
         with self.assertRaises(ValidationError):
             careplan.patient_id = self.patient_2
 
     def test_create_careplan(self):
-        encounter = self.env['medical.encounter'].create({
-            'patient_id': self.patient.id
-        })
+        encounter = self.env["medical.encounter"].create(
+            {"patient_id": self.patient.id}
+        )
         self.assertEqual(encounter.careplan_count, 0)
         res = encounter.action_view_careplans()
-        self.assertFalse(res.get('res_id'))
-        careplan = self.env['medical.careplan'].create({
-            'patient_id': self.patient.id,
-            'encounter_id': encounter.id
-        })
+        self.assertFalse(res.get("res_id"))
+        careplan = self.env["medical.careplan"].create(
+            {"patient_id": self.patient.id, "encounter_id": encounter.id}
+        )
         self.assertEqual(encounter.careplan_count, 1)
         res = encounter.action_view_careplans()
-        self.assertTrue(res.get('res_id'))
-        self.env['medical.careplan'].create({
-            'patient_id': self.patient.id,
-            'encounter_id': encounter.id
-        })
+        self.assertTrue(res.get("res_id"))
+        self.env["medical.careplan"].create(
+            {"patient_id": self.patient.id, "encounter_id": encounter.id}
+        )
         self.assertEqual(encounter.careplan_count, 2)
         res = encounter.action_view_careplans()
-        self.assertFalse(res.get('res_id'))
-        self.env['medical.careplan.add.plan.definition'].create({
-            'careplan_id': careplan.id,
-            'plan_definition_id': self.plan.id
-        }).run()
+        self.assertFalse(res.get("res_id"))
+        self.env["medical.careplan.add.plan.definition"].create(
+            {"careplan_id": careplan.id, "plan_definition_id": self.plan.id}
+        ).run()
