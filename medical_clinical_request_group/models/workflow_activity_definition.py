43aa9797845a6662bf6e20de17b1e047ae02969b
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_clinical_request_group/models/workflow_activity_definition.py
+++ medical_clinical_request_group/models/workflow_activity_definition.py
@@ -6,13 +6,15 @@ from odoo import models
 
 
 class ActivityDefinition(models.Model):
-    _inherit = 'workflow.activity.definition'
+    _inherit = "workflow.activity.definition"
 
-    def _get_medical_values(self, vals, parent=False, plan=False, action=False
-                            ):
+    def _get_medical_values(
+        self, vals, parent=False, plan=False, action=False
+    ):
         values = super(ActivityDefinition, self)._get_medical_values(
-            vals, parent, plan, action)
+            vals, parent, plan, action
+        )
 
-        if parent and parent._name == 'medical.request.group':
-            values['request_group_id'] = parent.id
+        if parent and parent._name == "medical.request.group":
+            values["request_group_id"] = parent.id
         return values
