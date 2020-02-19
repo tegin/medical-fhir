7c54b0b409b406c53dbb772af80db6d80005c4dd
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_medication_request/models/medical_medication_request.py
+++ medical_medication_request/models/medical_medication_request.py
@@ -9,118 +9,121 @@ from odoo.exceptions import ValidationError
 class MedicalMedicationRequest(models.Model):
     # FHIR entity: Medication request
     # (https://www.hl7.org/fhir/medicationrequest.html)
-    _name = 'medical.medication.request'
-    _description = 'Medical Medication request'
-    _inherit = 'medical.request'
+    _name = "medical.medication.request"
+    _description = "Medical Medication request"
+    _inherit = "medical.request"
 
-    internal_identifier = fields.Char(
-        string="Medication request"
-    )
+    internal_identifier = fields.Char(string="Medication request")
     category = fields.Selection(
         [
-            ('inpatient', 'Inpatient'),
-            ('outpatient', 'Outpatient'),
-            ('community', 'Community')
+            ("inpatient", "Inpatient"),
+            ("outpatient", "Outpatient"),
+            ("community", "Community"),
         ],
         required=True,
-        default='inpatient',
-    )   # FHIR Field: category
+        default="inpatient",
+    )  # FHIR Field: category
     product_id = fields.Many2one(
-        comodel_name='product.product',
-        domain=[('is_medication', '=', True)],
+        comodel_name="product.product",
+        domain=[("is_medication", "=", True)],
         required=True,
-        ondelete='restrict', index=True,
+        ondelete="restrict",
+        index=True,
     )
     product_uom_id = fields.Many2one(
-        'uom.uom',
-        'Unit of Measure',
+        "uom.uom",
+        "Unit of Measure",
         required=True,
-        ondelete='restrict', index=True,
+        ondelete="restrict",
+        index=True,
     )
-    qty = fields.Float(
-        'Quantity',
-        default=1.0,
-        required=True,
-    )
+    qty = fields.Float("Quantity", default=1.0, required=True)
     medication_administration_ids = fields.One2many(
-        comodel_name='medical.medication.administration',
-        inverse_name='medication_request_id',
+        comodel_name="medical.medication.administration",
+        inverse_name="medication_request_id",
     )
     medication_administration_count = fields.Integer(
         compute="_compute_medication_administration_count",
-        string='# of Medication Administration Requests',
+        string="# of Medication Administration Requests",
         copy=False,
         default=0,
     )
 
     @api.multi
-    @api.depends('medication_administration_ids')
+    @api.depends("medication_administration_ids")
     def _compute_medication_administration_count(self):
         for rec in self:
             rec.medication_administration_count = len(
-                rec.medication_administration_ids)
+                rec.medication_administration_ids
+            )
 
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def onchange_product_id(self):
         if self.product_id:
             self.product_uom_id = self.product_id.uom_id.id
 
     def _get_internal_identifier(self, vals):
-        return self.env['ir.sequence'].next_by_code(
-            'medical.medication.request') or '/'
+        return (
+            self.env["ir.sequence"].next_by_code("medical.medication.request")
+            or "/"
+        )
 
     def _get_event_values(self):
         return {
-            'medication_request_id': self.id,
-            'product_id': self.product_id.id,
-            'qty': self.qty,
-            'product_uom_id': self.product_uom_id.id,
-            'patient_id': self.patient_id.id,
-            'name': self.name,
+            "medication_request_id": self.id,
+            "product_id": self.product_id.id,
+            "qty": self.qty,
+            "product_uom_id": self.product_uom_id.id,
+            "patient_id": self.patient_id.id,
+            "name": self.name,
         }
 
     @api.multi
     def generate_event(self):
         self.ensure_one()
-        return self.env['medical.medication.administration'].create(
-            self._get_event_values())
+        return self.env["medical.medication.administration"].create(
+            self._get_event_values()
+        )
 
     @api.multi
     def action_view_medication_administration(self):
         self.ensure_one()
         action = self.env.ref(
-            'medical_medication_request.'
-            'medical_medication_administration_action')
+            "medical_medication_request."
+            "medical_medication_administration_action"
+        )
         result = action.read()[0]
-        result['context'] = {
-            'default_patient_id': self.patient_id.id,
-            'default_medication_request_id': self.id,
-            'default_name': self.name,
-            'default_product_id': self.product_id.id,
-            'default_product_uom_id': self.product_uom_id.id,
-            'default_qty': self.qty
+        result["context"] = {
+            "default_patient_id": self.patient_id.id,
+            "default_medication_request_id": self.id,
+            "default_name": self.name,
+            "default_product_id": self.product_id.id,
+            "default_product_uom_id": self.product_uom_id.id,
+            "default_qty": self.qty,
         }
-        result['domain'] = "[('medication_request_id', '=', " + \
-                           str(self.id) + ")]"
+        result["domain"] = (
+            "[('medication_request_id', '=', " + str(self.id) + ")]"
+        )
         if len(self.medication_administration_ids) == 1:
-            result['views'] = [(False, 'form')]
-            result['res_id'] = self.medication_administration_ids.id
+            result["views"] = [(False, "form")]
+            result["res_id"] = self.medication_administration_ids.id
         return result
 
     def _get_parent_field_name(self):
-        return 'medication_request_id'
+        return "medication_request_id"
 
     def action_view_request_parameters(self):
         return {
-            'view': 'medical_medication_request.'
-                    'medical_medication_request_action',
-            'view_form': 'medical.medication.request.view.form', }
+            "view": "medical_medication_request."
+            "medical_medication_request_action",
+            "view_form": "medical.medication.request.view.form",
+        }
 
-    @api.constrains('patient_id')
+    @api.constrains("patient_id")
     def _check_patient_medication(self):
 
-        if not self.env.context.get('no_check_patient', False):
+        if not self.env.context.get("no_check_patient", False):
             if self.medication_administration_ids.filtered(
                 lambda r: r.patient_id != self.patient_id
             ):
-                raise ValidationError(_('Patient inconsistency'))
+                raise ValidationError(_("Patient inconsistency"))
--- medical_medication_request/models/procurement.py
+++ medical_medication_request/models/procurement.py
@@ -2,17 +2,31 @@ from odoo import models
 
 
 class StockRule(models.Model):
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
     def _get_stock_move_values(
-        self, product_id, product_qty, product_uom, location_id, name,
-        origin, values, group_id
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        values,
+        group_id,
     ):
         res = super()._get_stock_move_values(
-            product_id, product_qty, product_uom, location_id, name,
-            origin, values, group_id
+            product_id,
+            product_qty,
+            product_uom,
+            location_id,
+            name,
+            origin,
+            values,
+            group_id,
         )
-        if values.get('medication_administration_id', False):
-            res['medication_administration_id'] = values.get(
-                'medication_administration_id')
+        if values.get("medication_administration_id", False):
+            res["medication_administration_id"] = values.get(
+                "medication_administration_id"
+            )
         return res
