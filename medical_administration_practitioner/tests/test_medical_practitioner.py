80045598c4c0aaf1245723b2c3a20eaf6f43a0f1
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_administration_practitioner/tests/test_medical_practitioner.py
+++ medical_administration_practitioner/tests/test_medical_practitioner.py
@@ -7,22 +7,18 @@ from odoo.tests.common import TransactionCase
 
 
 class TestMedicalAdministrationPractitioner(TransactionCase):
-
     def setUp(self):
         super(TestMedicalAdministrationPractitioner, self).setUp()
-        self.practitioner_model = self.env['res.partner']
-        self.role_model = self.env['medical.role']
+        self.practitioner_model = self.env["res.partner"]
+        self.role_model = self.env["medical.role"]
 
     def test_security(self):
-        role_vals = {
-            'name': 'Nurse',
-            'description': 'Nurse'
-        }
+        role_vals = {"name": "Nurse", "description": "Nurse"}
         role_1 = self.role_model.create(role_vals)
         practitioner_vals = {
-            'is_practitioner': True,
-            'name': 'Nurse X',
-            'practitioner_role_ids': [(6, 0, role_1.ids)]
+            "is_practitioner": True,
+            "name": "Nurse X",
+            "practitioner_role_ids": [(6, 0, role_1.ids)],
         }
         practitioner_1 = self.practitioner_model.create(practitioner_vals)
         self.assertNotEquals(practitioner_1, False)
