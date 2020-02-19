cf673ca3c78f88882889ddb4788616f212813c25
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_medication_request/models/stock_move.py
+++ medical_medication_request/models/stock_move.py
@@ -6,16 +6,17 @@ from odoo import api, fields, models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     medication_administration_id = fields.Many2one(
-        comodel_name='medical.medication.administration',
-        string='Medication administration event',
-        ondelete='restrict', index=True,
+        comodel_name="medical.medication.administration",
+        string="Medication administration event",
+        ondelete="restrict",
+        index=True,
     )
 
     @api.model
     def _prepare_merge_moves_distinct_fields(self):
         distinct_fields = super()._prepare_merge_moves_distinct_fields()
-        distinct_fields.append('medication_administration_id')
+        distinct_fields.append("medication_administration_id")
         return distinct_fields
--- medical_medication_request/models/workflow_activity_definition.py
+++ medical_medication_request/models/workflow_activity_definition.py
@@ -8,15 +8,19 @@ from odoo import models
 class ActivityDefinition(models.Model):
     # FHIR entity: Activity Definition
     # (https://www.hl7.org/fhir/activitydefinition.html)
-    _inherit = 'workflow.activity.definition'
+    _inherit = "workflow.activity.definition"
 
-    def _get_medical_values(self, vals, parent=False, plan=False, action=False
-                            ):
+    def _get_medical_values(
+        self, vals, parent=False, plan=False, action=False
+    ):
         values = super(ActivityDefinition, self)._get_medical_values(
-            vals, parent, plan, action)
-        if self.model_id.model == 'medical.medication.request':
-            values.update({
-                'product_id': self.service_id.id,
-                'product_uom_id': self.service_id.uom_id.id,
-            })
+            vals, parent, plan, action
+        )
+        if self.model_id.model == "medical.medication.request":
+            values.update(
+                {
+                    "product_id": self.service_id.id,
+                    "product_uom_id": self.service_id.uom_id.id,
+                }
+            )
         return values
