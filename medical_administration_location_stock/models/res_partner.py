35b2fef5bb0c867e55c22cefac2bf6fc0e296da3
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_administration_location_stock/models/res_partner.py
+++ medical_administration_location_stock/models/res_partner.py
@@ -7,12 +7,8 @@ from odoo import fields, models
 
 class ResPartner(models.Model):
     # FHIR Entity: Location (https://www.hl7.org/fhir/location.html)
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    stock_location_id = fields.Many2one(
-        comodel_name='stock.location',
-    )
+    stock_location_id = fields.Many2one(comodel_name="stock.location")
 
-    stock_picking_type_id = fields.Many2one(
-        comodel_name='stock.picking.type'
-    )
+    stock_picking_type_id = fields.Many2one(comodel_name="stock.picking.type")
