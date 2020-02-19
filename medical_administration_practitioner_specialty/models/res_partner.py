89e7305e21e822764de637a9982e5869c9dd1e5d
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_administration_practitioner_specialty/models/res_partner.py
+++ medical_administration_practitioner_specialty/models/res_partner.py
@@ -9,9 +9,6 @@ from odoo import fields, models
 class ResPartner(models.Model):
     # FHIR Entity: PractitionerRole
     # (https://www.hl7.org/fhir/practitionerrole.html)
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    specialty_ids = fields.Many2many(
-        'medical.specialty',
-        string='Specialty',
-    )
+    specialty_ids = fields.Many2many("medical.specialty", string="Specialty")
