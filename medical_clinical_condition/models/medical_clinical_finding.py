c8986ed01c5904acaa82289784425a355ebad109
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_clinical_condition/models/medical_clinical_finding.py
+++ medical_clinical_condition/models/medical_clinical_finding.py
@@ -8,20 +8,20 @@ from odoo import api, fields, models
 class MedicalClinicalFinding(models.Model):
     # FHIR Entity: Condition Code
     # (http://hl7.org/fhir/valueset-condition-code.html)
-    _name = 'medical.clinical.finding'
-    _inherit = 'medical.abstract'
-    _description = 'Condition/Problem/Diagnosis codes'
+    _name = "medical.clinical.finding"
+    _inherit = "medical.abstract"
+    _description = "Condition/Problem/Diagnosis codes"
 
-    name = fields.Char(
-        required=True,
-    )
+    name = fields.Char(required=True)
     description = fields.Char()
     sct_code_id = fields.Many2one(
-        comodel_name='medical.sct.concept',
-        domain=[('is_clinical_finding', '=', True)],
+        comodel_name="medical.sct.concept",
+        domain=[("is_clinical_finding", "=", True)],
     )
 
     @api.model
     def _get_internal_identifier(self, vals):
-        return self.env['ir.sequence'].next_by_code(
-            'medical.clinical.finding') or '/'
+        return (
+            self.env["ir.sequence"].next_by_code("medical.clinical.finding")
+            or "/"
+        )
