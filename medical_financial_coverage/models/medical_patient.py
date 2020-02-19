1c7c058c1c835623f1d24d5fbce96a878f98532e
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_financial_coverage/models/medical_patient.py
+++ medical_financial_coverage/models/medical_patient.py
@@ -7,21 +7,21 @@ from odoo import api, fields, models
 
 class MedicalPatient(models.Model):
     # FHIR Entity: Patient (http://hl7.org/fhir/patient.html)
-    _inherit = 'medical.patient'
+    _inherit = "medical.patient"
 
     coverage_ids = fields.One2many(
         string="Coverage",
-        comodel_name='medical.coverage',
+        comodel_name="medical.coverage",
         inverse_name="patient_id",
     )
     coverage_count = fields.Integer(
         compute="_compute_coverage_count",
-        string='# of Coverages',
+        string="# of Coverages",
         copy=False,
         default=0,
     )
 
-    @api.depends('coverage_ids')
+    @api.depends("coverage_ids")
     def _compute_coverage_count(self):
         for record in self:
             record.coverage_count = len(record.coverage_ids)
@@ -29,12 +29,13 @@ class MedicalPatient(models.Model):
     @api.multi
     def action_view_coverage(self):
         action = self.env.ref(
-            'medical_financial_coverage.medical_coverage_action')
+            "medical_financial_coverage.medical_coverage_action"
+        )
         result = action.read()[0]
-        result['context'] = {'default_patient_id': self.id}
-        result['domain'] = "[('patient_id', '=', " + str(self.id) + ")]"
+        result["context"] = {"default_patient_id": self.id}
+        result["domain"] = "[('patient_id', '=', " + str(self.id) + ")]"
         if len(self.coverage_ids) == 1:
-            res = self.env.ref('medical.coverage.view.form', False)
-            result['views'] = [(res and res.id or False, 'form')]
-            result['res_id'] = self.coverage_ids.id
+            res = self.env.ref("medical.coverage.view.form", False)
+            result["views"] = [(res and res.id or False, "form")]
+            result["res_id"] = self.coverage_ids.id
         return result
