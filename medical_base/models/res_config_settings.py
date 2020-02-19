77fc10f5c0accc1bffabd9e12b939f2fe7e43035
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:40:07 +0100

    Blacking all

--- medical_base/models/res_config_settings.py
+++ medical_base/models/res_config_settings.py
@@ -6,12 +6,12 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
-    module_medical_clinical = fields.Boolean('Clinical')
-    module_medical_workflow = fields.Boolean('Workflow')
-    module_medical_administration = fields.Boolean('Administration')
-    module_medical_financial = fields.Boolean('Financial')
-    module_medical_medication = fields.Boolean('Medication')
-    module_medical_diagnostics = fields.Boolean('Diagnostics')
-    module_medical_terminology = fields.Boolean('Terminologies')
+    module_medical_clinical = fields.Boolean("Clinical")
+    module_medical_workflow = fields.Boolean("Workflow")
+    module_medical_administration = fields.Boolean("Administration")
+    module_medical_financial = fields.Boolean("Financial")
+    module_medical_medication = fields.Boolean("Medication")
+    module_medical_diagnostics = fields.Boolean("Diagnostics")
+    module_medical_terminology = fields.Boolean("Terminologies")
