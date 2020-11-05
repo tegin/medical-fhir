20fee486dd439b3b56510170b45b84ef98fc3f0e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-11-05 14:03:57 +0100

    [FIX] new black

--- medical_medication_request/models/medical_medication_request.py
+++ medical_medication_request/models/medical_medication_request.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
