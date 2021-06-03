5062720f8974d32efc63354b66f82a317d6df6a0
Enric Tobella <etobella@creublanca.es>
2021-06-03 15:00:23 +0200

    [IMP] githubactions: Copy logic from OCA

--- medical_administration_center/__manifest__.py
+++ medical_administration_center/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Creu Blanca
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Medical Administration Location",
@@ -8,7 +8,7 @@
     "category": "Medical",
     "website": "https://github.com/OCA/vertical-medical",
     "author": "Creu Blanca, Eficent",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": ["medical_administration_location"],
     "data": [
         "security/medical_security.xml",
--- medical_administration_center/models/res_partner.py
+++ medical_administration_center/models/res_partner.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Creu Blanca
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
