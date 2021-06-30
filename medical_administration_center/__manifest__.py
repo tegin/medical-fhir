ea21220446332e545ca40b3ffb2cca1801f9e75d
Olga Marco <olga.marco@creublanca.es>
2021-06-30 12:40:00 +0200

    [MIG] cb_medical_administration_center: Migration to 13.0

--- medical_administration_center/__manifest__.py
+++ medical_administration_center/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Medical Administration Location",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Medical",
     "website": "https://github.com/OCA/vertical-medical",
     "author": "Creu Blanca, Eficent",
