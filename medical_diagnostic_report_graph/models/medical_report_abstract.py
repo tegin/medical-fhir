3024d2a42b6d6f49fcf452008035fb0ea266b8d4
Alba Riera <alba.riera@creublanca.es>
2021-10-18 09:48:06 +0200

    [ADD] medical_diagnostic_report_graph: create graph inside a report

---
+++ medical_diagnostic_report_graph/models/medical_report_abstract.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class MedicalReportAbstract(models.AbstractModel):
+
+    _inherit = "medical.report.abstract"
+
+    compute_graph = fields.Boolean()
+    compute_html = fields.Boolean()
+
+    hide_observations = fields.Boolean(help="Hide observations on report")
