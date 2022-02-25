ff82211e94d5203550df53394ee963425513685e
Enric Tobella <etobella@creublanca.es>
2022-02-25 11:02:21 +0100

    [FIX] medical_diagnostic_report: Integration with hr failed

--- medical_diagnostic_report/models/res_users.py
+++ medical_diagnostic_report/models/res_users.py
@@ -4,3 +4,23 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = "res.users"
     digital_signature = fields.Binary(attachment=True)
+
+    def __init__(self, pool, cr):  # pylint: disable=E0101
+        """ Override of __init__ to add access rights.
+            Access rights are disabled by default, but allowed
+            on some specific fields defined in self.SELF_{READ/WRITE}ABLE_FIELDS.
+        """
+
+        hr_writable_fields = [
+            "digital_signature",
+        ]
+
+        init_res = super().__init__(pool, cr)
+        # duplicate list to avoid modifying the original reference
+        type(self).SELF_READABLE_FIELDS = (
+            type(self).SELF_READABLE_FIELDS + hr_writable_fields
+        )
+        type(self).SELF_WRITEABLE_FIELDS = (
+            type(self).SELF_WRITEABLE_FIELDS + hr_writable_fields
+        )
+        return init_res
