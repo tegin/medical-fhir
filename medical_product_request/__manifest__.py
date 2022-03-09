dec59bc4829ee0f8fd5d2117280183804656819f
Alba Riera <alba.riera@creublanca.es>
2022-03-09 18:02:58 +0100

    [ADD] medical_product_request

---
+++ medical_product_request/__manifest__.py
@@ -0,0 +1,35 @@
+# Copyright 2022 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Medical Product Request",
+    "summary": """
+        This addon sets the base of the medical fhir concepts
+        of medication.request and device.request""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "www.creublanca.es",
+    "depends": [
+        "medical_administration_encounter",
+        "mail",
+        "medical_clinical",
+        "medical_administration_practitioner",
+        "web_domain_field",
+    ],
+    "data": [
+        "security/medical_security.xml",
+        "security/ir.model.access.csv",
+        "views/medical_patient.xml",
+        "views/medical_encounter.xml",
+        "data/ir_sequence_data.xml",
+        "views/medical_product_template.xml",
+        "views/medical_product_product.xml",
+        "views/medical_product_administration.xml",
+        "views/medical_product_request_order.xml",
+        "views/medical_product_request.xml",
+        "views/medication_form.xml",
+        "views/medical_administration_route.xml",
+    ],
+    "demo": ["demo/medical_product_request_demo.xml"],
+}
