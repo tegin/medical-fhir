7f0de09fea6a3f3d92d8e1a90b9aae2b927b028f
Alba Riera <alba.riera@creublanca.es>
2022-04-21 11:00:36 +0200

    [ADD] medical_clinical_impression

---
+++ medical_clinical_impression/models/medical_condition.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Creu Blanca
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class MedicalCondition(models.Model):
+
+    _name = "medical.condition"
+    _inherit = "medical.condition"
+
+    origin_clinical_impression_id = fields.Many2one(
+        comodel_name="medical.clinical.impression"
+    )
+    # This is used to keep track of the condition in case
+    # the related impression is cancelled
