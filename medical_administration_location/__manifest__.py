aab3e6974320d8d43c614159b91ce81052dd58e9
Enric Tobella <etobella@creublanca.es>
2022-11-04 10:21:45 +0100

    [MIG] Migration to 14.0

--- medical_administration_location/__manifest__.py
+++ medical_administration_location/__manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2017 LasLabs Inc.
-# Copyright 2017 Creu Blanca
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017 CreuBlanca
+# Copyright 2017 ForgeFlow
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Medical Administration Location",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Medical",
-    "website": "https://github.com/OCA/vertical-medical",
-    "author": "LasLabs, Creu Blanca, Eficent,"
+    "website": "https://github.com/tegin/medical-fhir",
+    "author": "LasLabs, CreuBlanca, ForgeFlow,"
     "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "depends": ["medical_administration"],
