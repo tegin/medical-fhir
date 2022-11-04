6bf899b7a86b1cf75b9ccdc791d6a8de78c93517
Enric Tobella <etobella@creublanca.es>
2022-11-04 10:21:45 +0100

    [MIG] Migration to 14.0

--- medical_certify/__manifest__.py
+++ medical_certify/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2020 Creu Blanca
+# Copyright 2020 CreuBlanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Medical Certify",
     "summary": """
         Certify medical entities""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/medical-fhir",
     "depends": ["report_qr"],
     "data": ["views/certify_base.xml"],
