6c0e95c96ce26acc905febf56b0369dc556dd232
Enric Tobella <etobella@creublanca.es>
2022-11-09 10:02:08 +0100

    [REF] medical_fhir

--- medical_clinical_request_group/tests/test_request_group.py
+++ medical_clinical_request_group/tests/test_request_group.py
@@ -8,7 +8,7 @@ from odoo.tests import TransactionCase
 class TestRequestGroup(TransactionCase):
     def setUp(self):
         res = super(TestRequestGroup, self).setUp()
-        self.patient = self.browse_ref("medical_administration.patient_01")
+        self.patient = self.browse_ref("medical_base.patient_01")
         self.plan = self.browse_ref("medical_workflow.basic_check_up")
         return res
 
@@ -17,23 +17,17 @@ class TestRequestGroup(TransactionCase):
             {"patient_id": self.patient.id}
         )
         self.assertNotEqual(request.internal_identifier, "/")
-        self.assertTrue(request.is_editable)
-        self.assertEqual(request.state, "draft")
+        self.assertEqual(request.fhir_state, "draft")
         request.draft2active()
-        self.assertFalse(request.is_editable)
-        self.assertEqual(request.state, "active")
+        self.assertEqual(request.fhir_state, "active")
         request.active2suspended()
-        self.assertFalse(request.is_editable)
-        self.assertEqual(request.state, "suspended")
+        self.assertEqual(request.fhir_state, "suspended")
         request.reactive()
         request.active2error()
-        self.assertFalse(request.is_editable)
-        self.assertEqual(request.state, "entered-in-error")
+        self.assertEqual(request.fhir_state, "entered-in-error")
         request.reactive()
         request.active2completed()
-        self.assertFalse(request.is_editable)
-        self.assertEqual(request.state, "completed")
+        self.assertEqual(request.fhir_state, "completed")
         request.reactive()
         request.cancel()
-        self.assertFalse(request.is_editable)
-        self.assertEqual(request.state, "cancelled")
+        self.assertEqual(request.fhir_state, "cancelled")
