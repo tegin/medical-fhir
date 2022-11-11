ffb4b00476d42b759c37c486d0d063433278dde0
Enric Tobella <etobella@creublanca.es>
2022-11-11 12:22:32 +0100

    [FIX] medical_diagnostic_report: Remove measure_types

--- medical_medication_request/models/medical_medication_request.py
+++ medical_medication_request/models/medical_medication_request.py
@@ -70,7 +70,9 @@ class MedicalMedicationRequest(models.Model):
 
     def _get_internal_identifier(self, vals):
         return (
-            self.env["ir.sequence"].next_by_code("medical.medication.request")
+            self.env["ir.sequence"]
+            .sudo()
+            .next_by_code("medical.medication.request")
             or "/"
         )
 
