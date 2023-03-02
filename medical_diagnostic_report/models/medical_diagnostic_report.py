b46555c147e3e837787793baf731a9bcaa3dadc5
Núria Sancho <nuria.sancho@creublanca.es>
2023-03-02 18:02:00 +0100

    [14.0][IMP] medical_diagnostic_report: Add pdf button in tree report diagnostic
    [14.0][IMP] medical_diagnostic_report: Add pdf button in tree report 2.0

--- medical_diagnostic_report/models/medical_diagnostic_report.py
+++ medical_diagnostic_report/models/medical_diagnostic_report.py
@@ -212,3 +212,8 @@ class MedicalDiagnosticReport(models.Model):
             }
         )
         return result
+
+    def print_medical_diagnostic_report(self):
+        return self.env.ref(
+            "medical_diagnostic_report.medical_diagnostic_report"
+        ).report_action(self)
