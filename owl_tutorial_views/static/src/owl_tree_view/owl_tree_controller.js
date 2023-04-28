5990ed2fb7d6bc487f62df5287b90a4a2b22a4ad
Núria Sancho <nuria.sancho@creublanca.es>
2023-04-28 09:34:01 +0200

    [14.0][ADD] owl_tutorial_views: Add parameters in owl views impression

--- owl_tutorial_views/static/src/owl_tree_view/owl_tree_controller.js
+++ owl_tutorial_views/static/src/owl_tree_view/owl_tree_controller.js
@@ -4,10 +4,7 @@ odoo.define("owl_tutorial_views.OWLTreeController", function (require) {
     var AbstractController = require("web.AbstractController");
 
     var OWLTreeController = AbstractController.extend({
-        custom_events: _.extend({}, AbstractController.prototype.custom_events, {
-
-        }),
-
+        custom_events: _.extend({}, AbstractController.prototype.custom_events, {}),
     });
 
     return OWLTreeController;
