a19482831f9472087f99a866ace0cb5d757c58b1
Enric Tobella <etobella@creublanca.es>
2023-05-15 20:06:25 +0200

    [IMP] medical_base: add modification in order to execute an action on click

--- medical_base/__manifest__.py
+++ medical_base/__manifest__.py
@@ -19,6 +19,7 @@
         "views/medical_menu.xml",
         "views/medical_patient.xml",
         "views/res_config_settings_views.xml",
+        "templates/assets.xml",
     ],
     "demo": ["demo/medical_demo.xml"],
     "application": True,
