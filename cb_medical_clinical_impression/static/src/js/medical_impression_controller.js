81049e542caa8e121ce20af01fce489906d41d55
Núria Sancho <nuria.sancho@creublanca.es>
2023-06-02 14:01:02 +0200

    fix names

--- cb_medical_clinical_impression/static/src/js/medical_impression_controller.js
+++ cb_medical_clinical_impression/static/src/js/medical_impression_controller.js
@@ -11,6 +11,7 @@ odoo.define("cb_medical_clinical_impression.MedicalImpressionController", functi
             MedicalImpressionController.prototype.custom_events,
             {
                 create_impression_report: "_onCreateReportImpression",
+                update_routine_medication: "updateRoutineMedication",
             }
         ),
         _onCreateReportImpression(ev) {
@@ -22,6 +23,16 @@ odoo.define("cb_medical_clinical_impression.MedicalImpressionController", functi
                 args: [[ev.data.res_id]],
             }).then(function (action) {
                 self.do_action(action);
+            });
+        },
+        updateRoutineMedication(ev) {
+            var self = this;
+            self._rpc({
+                model: "medical.patient",
+                method: "set_routine_medication",
+                args: [[ev.data.res_id], ev.data.routine_medication],
+            }).then(function (data) {
+                self.updatePatientInfo();
             });
         },
     });
