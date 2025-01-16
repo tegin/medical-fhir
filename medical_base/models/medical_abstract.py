a0afb2eca5c01c2aa7d6673c40c5f6902c74a5e3
Enric Tobella <enric.tobella@dixmit.com>
2025-01-16 22:14:02 +0100

    [FIX] medical_base: Remove warnings

--- medical_base/models/medical_abstract.py
+++ medical_base/models/medical_abstract.py
@@ -19,12 +19,12 @@ class MedicalAbstract(models.AbstractModel):
         copy=False,
     )  # FHIR Field: identifier
 
-    @api.model
-    def create(self, vals):
-        vals_upd = vals.copy()
-        if vals_upd.get("internal_identifier", "/") == "/":
-            vals_upd["internal_identifier"] = self._get_internal_identifier(vals_upd)
-        return super(MedicalAbstract, self).create(vals_upd)
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get("internal_identifier", "/") == "/":
+                vals["internal_identifier"] = self._get_internal_identifier(vals)
+        return super().create(vals_list)
 
     def _get_internal_identifier(self, vals):
         # It should be rewritten for each element
