52d68b8637239cb43d113dbaaa5343c5aadba689
Enric Tobella <enric.tobella@dixmit.com>
2025-01-16 20:33:16 +0100

    [IMP] medical_clincial_request_group: Black, isort, prettier

--- medical_clinical_request_group/models/workflow_activity_definition.py
+++ medical_clinical_request_group/models/workflow_activity_definition.py
@@ -11,9 +11,7 @@ class ActivityDefinition(models.Model):
     def _get_medical_models(self):
         return super()._get_medical_models() + ["medical.request.group"]
 
-    def _get_medical_values(
-        self, vals, parent=False, plan=False, action=False
-    ):
+    def _get_medical_values(self, vals, parent=False, plan=False, action=False):
         values = super(ActivityDefinition, self)._get_medical_values(
             vals, parent, plan, action
         )
--- medical_clinical_request_group/tests/test_medical_request.py
+++ medical_clinical_request_group/tests/test_medical_request.py
@@ -9,12 +9,8 @@ from odoo.tests.common import TransactionCase
 class TestMedicalRequest(TransactionCase):
     def setUp(self):
         super(TestMedicalRequest, self).setUp()
-        self.patient = self.env["medical.patient"].create(
-            {"name": "Test Patient"}
-        )
-        self.patient2 = self.env["medical.patient"].create(
-            {"name": "Test Patient2"}
-        )
+        self.patient = self.env["medical.patient"].create({"name": "Test Patient"})
+        self.patient2 = self.env["medical.patient"].create({"name": "Test Patient2"})
 
     def test_constrains(self):
         request = self.env["medical.request.group"].create(
